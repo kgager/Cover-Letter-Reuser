b4c1092c42b8ffdbd47d648ed6184503fbdb0c6f
warren <warrendlee1@gmail.com>
2021-09-05 00:07:59 -0700

    pdf feature

--- CoverLetterCompleter/index.js
+++ CoverLetterCompleter/index.js
@@ -6,13 +6,29 @@ function fillInCompany() {
     document.getElementById("updatedCoverLetter").value = updatedCoverLetter
 }
 
-function writePDF(letter) {
-    console.log(jspdf)
-    const doc = new jspdf.jsPDF({
-        format: [100, 100]
-    })
-    doc.text(letter, 1, 1, 0, 1);
-    doc.save("coverLetter.pdf");
-}
+async function writePDF(letter) {
+    if (letter === null || letter === "") {
+        alert("No cover letter detected!")
+    } else {
+        const { PDFDocument, StandardFonts, rgb } = PDFLib
+        const pdfDoc = await PDFDocument.create()
+        const Helvetica = await pdfDoc.embedFont(StandardFonts.Helvetica)
+        const page = pdfDoc.addPage()
+        const { height } = page.getSize()
+        const fontSize = 12
 
+        page.drawText(letter, {
+            x: 50,
+            y: height - 4 * fontSize,
+            size: fontSize,
+            font: Helvetica,
+            maxWidth: 500,
+            color: rgb(0,0,0),
+            lineHeight: 20,
+        })
+
+        const pdfBytes = await pdfDoc.save()
+        download(pdfBytes, "CoverLetter.pdf", "application/pdf");
+    }
+}
 
